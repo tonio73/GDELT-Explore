4e01d3e92bcded3c764b00cb8c33b2c3a89e5336
Axel Camara <axel.camara@hotmail.fr>
2020-01-20 18:50:55 +0100

    add code to write into cassandra via spark

--- src/main/scala/MainQueryB.scala
+++ src/main/scala/MainQueryB.scala
@@ -3,6 +3,7 @@ package fr.telecom
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions._
+import com.datastax.spark.connector._
 
 object MainQueryB extends App {
   // Select files corresponding to reference period (as set in Context.scala)
@@ -24,11 +25,17 @@ object MainQueryB extends App {
   val country = "FR"
 
   // First data to put into cassandra, the country parameter must be given to cassandra and not spark (need to do filter in cassandra)
-  val res = eventsDs.as("events").join(mentionsDs.as("mentions"), $"events.GLOBALEVENTID" === $"mentions.GLOBALEVENTID", joinType="left" )
+  val res = eventsDs.as("events").join(mentionsDs.as("mentions"), $"events.GLOBALEVENTID" === $"mentions.GLOBALEVENTID", joinType = "left")
     .groupBy($"events.GLOBALEVENTID").count()
-    .join(eventsDs, Seq("GLOBALEVENTID"), joinType="left")
+    .join(eventsDs, Seq("GLOBALEVENTID"), joinType = "left")
     .orderBy(col("count").desc)
 
+
+
   // Currently to CSV, later to Cassandra
-  res.write.mode("overwrite").csv(Context.outputPath + "/reqB_csv")
+  // res.write.mode("overwrite").csv(Context.outputPath + "/reqB_csv")
+  val columnNames = Seq("GLOBALEVENTID", "SQLDATE", "count")
+  res.select(columnNames.map(c => col(c)): _*).rdd.saveToCassandra("test", "queryb", SomeColumns("globaleventid", "sqldate", "count"))
+
+  println("Finish request b)")
 }
