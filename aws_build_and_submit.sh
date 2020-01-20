44b91f17c133267870b5af3fa1ab7f2f10389f27
Axel Camara <axel.camara@hotmail.fr>
2020-01-20 19:33:32 +0100

    update query b to run on aws

--- aws_build_and_submit.sh
+++ aws_build_and_submit.sh
@@ -8,9 +8,9 @@ sbt assembly
 
 echo -e "\n --- upload .jar --- \n"
 
-aws s3 cp target/scala-2.11/GDELT-Explore-assembly-0.1.0.jar s3://fufu-program/jars/
+aws2 s3 cp target/scala-2.11/GDELT-Explore-assembly-0.1.0.jar s3://fufu-program/jars/
 
 echo -e "\n --- spark-submit through EMR step --- \n"
 
-aws emr add-steps --cluster-id $1 --steps "file://$2"
+aws2 emr add-steps --cluster-id $1 --steps "file://$2"
 
--- src/main/scala/MainQueryB.scala
+++ src/main/scala/MainQueryB.scala
@@ -1,41 +1,88 @@
 package fr.telecom
 
+import com.amazonaws.{AmazonServiceException, SdkClientException}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions._
 import com.datastax.spark.connector._
 
 object MainQueryB extends App {
-  // Select files corresponding to reference period (as set in Context.scala)
-  val spark = Context.createSession()
+  val logger = Context.logger
 
-  import spark.implicits._
+  try {
 
-  // Read GDELT compressed CSV (currently from /tmp/data, later from S3)
-  val eventsRDD: RDD[String] = Downloader.zipsToRdd(spark, Context.refPeriod() + "[0-9]*.export.CSV.zip")
-  val mentionsRDD: RDD[String] = Downloader.zipsToRdd(spark, Context.refPeriod() + "[0-9]*.mentions.CSV.zip")
+    var localMaster = false
+    var fromS3 = false
+    var cassandraIp = ""
+    var i: Int = 0
+    while (i < args.length) {
+      args(i) match {
+        case "--local-master" => localMaster = true
 
-  val eventsDs: Dataset[Event] = Event.rddToDs(spark, eventsRDD)
-  val mentionsDs: Dataset[Mention] = Mention.rddToDs(spark, mentionsRDD)
+        case "--from-s3" => fromS3 = true
 
-  // println("For %s, number of events = %d, number of mentions = %d".format(Context.refPeriod(), eventsDs.count(), mentionsDs.count()))
+        case "--cassandra-ip" => {
+          i += 1
+          cassandraIp = args(i)
+        }
 
-  // Request a) afficher le nombre d’articles/évènements qu’il y a eu pour chaque triplet (jour, pays de l’évènement, langue de l’article).
-  println("Launch request b)")
-  val country = "FR"
+        case _ => {
+          print("Unknown argument " + args(i) + "\n")
+          print("Usage: --index to download master files\n")
+        }
+      }
+      i += 1
+    }
 
-  // First data to put into cassandra, the country parameter must be given to cassandra and not spark (need to do filter in cassandra)
-  val res = eventsDs.as("events").join(mentionsDs.as("mentions"), $"events.GLOBALEVENTID" === $"mentions.GLOBALEVENTID", joinType = "left")
-    .groupBy($"events.GLOBALEVENTID").count()
-    .join(eventsDs, Seq("GLOBALEVENTID"), joinType = "left")
-    .orderBy(col("count").desc)
+    logger.info("Create Spark session")
+    // Select files corresponding to reference period (as set in Context.scala)
+    val spark = Context.createSession(localMaster, cassandraIp)
+
+    import spark.implicits._
+
+    // Read GDELT compressed CSV (currently from /tmp/data, later from S3)
+    val eventsRDD: RDD[String] = Downloader.zipsToRdd(spark, Context.refPeriod() + "[0-9]*.export.CSV.zip", fromS3)
+    val mentionsRDD: RDD[String] = Downloader.zipsToRdd(spark, Context.refPeriod() + "[0-9]*.mentions.CSV.zip", fromS3)
+
+    val eventsDs: Dataset[Event] = Event.rddToDs(spark, eventsRDD)
+    val mentionsDs: Dataset[Mention] = Mention.rddToDs(spark, mentionsRDD)
+
+    // println("For %s, number of events = %d, number of mentions = %d".format(Context.refPeriod(), eventsDs.count(), mentionsDs.count()))
+
+    // Request a) afficher le nombre d’articles/évènements qu’il y a eu pour chaque triplet (jour, pays de l’évènement, langue de l’article).
+    logger.info("Launch request b)")
+    val country = "FR"
+
+    // First data to put into cassandra, the country parameter must be given to cassandra and not spark (need to do filter in cassandra)
+    val res = eventsDs.as("events").join(mentionsDs.as("mentions"), $"events.GLOBALEVENTID" === $"mentions.GLOBALEVENTID", joinType = "left")
+      .groupBy($"events.GLOBALEVENTID").count()
+      .join(eventsDs, Seq("GLOBALEVENTID"), joinType = "left")
+      .orderBy(col("count").desc)
 
 
+    // Currently to CSV, later to Cassandra
+    /*if(fromS3) {
+      res.write.mode("overwrite").csv(Context.getS3Path(Context.bucketOutputPath + "reqB_csv"))
+    }
+    else {
+      res.write.mode("overwrite").csv(Context.outputPath + "reqB_csv")
+    }
 
-  // Currently to CSV, later to Cassandra
-  // res.write.mode("overwrite").csv(Context.outputPath + "/reqB_csv")
-  val columnNames = Seq("GLOBALEVENTID", "SQLDATE", "count")
-  res.select(columnNames.map(c => col(c)): _*).rdd.saveToCassandra("test", "queryb", SomeColumns("globaleventid", "sqldate", "count"))
+    logger.info("Completed write of request a)")*/
+    val columnNames = Seq("GLOBALEVENTID", "SQLDATE", "count")
+    res.select(columnNames.map(c => col(c)): _*).rdd.saveToCassandra("test", "queryb", SomeColumns("globaleventid", "sqldate", "count"))
 
-  println("Finish request b)")
+
+    logger.info("Finish request b)")
+  }
+  catch {
+    // The call was transmitted successfully, but AWS couldn't process
+    // it, so it returned an error response.
+    case e: AmazonServiceException => e.printStackTrace();
+    // Amazon S3 couldn't be contacted for a response, or the client
+    // couldn't parse the response from AWS.
+    case e: SdkClientException => e.printStackTrace();
+  }
+
+  logger.info("Fu-fu program completed")
 }
