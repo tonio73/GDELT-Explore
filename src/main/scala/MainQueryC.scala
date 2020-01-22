e21aa0e17a94c9033a39118d5359c4b1b15345dd
Antoine Hue <antoine.hue@gmail.com>
2020-01-22 18:01:00 +0100

    Improvements to MainQueryC to comply with /tmp data pull, structured the three subqueries

--- src/main/scala/MainQueryC.scala
+++ src/main/scala/MainQueryC.scala
@@ -1,122 +1,116 @@
 package fr.telecom
 
-import org.apache.spark.sql.DataFrame
+import com.amazonaws.{AmazonServiceException, SdkClientException}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions._
 
 import scala.collection.mutable.ArrayBuffer
 
 object MainQueryC extends App {
 
-  // Select files corresponding to reference period (as set in Context.scala)
-  val spark = Context.createSession()
-  val sc = spark.sparkContext
+  val logger = Context.logger
 
-  val path = "20150222091500.gkg.csv.zip"
+  try {
 
-  val gkgRDD = Downloader.zipsToRdd(spark, path, "/home/mde/Documents/Projet_Furet/")
-  val gkgDs = GKG.rddToDs(spark, gkgRDD)
+    logger.info("Create Spark session")
 
+    // Select files corresponding to reference period (as set in Context.scala)
+    val spark = Context.createSession()
 
-  val themeDs: DataFrame = gkgDs
-    //.drop(gkgDs.col("SourceCollectionIdentifier"))
-    .drop(gkgDs.col("DocumentIdentifier"))
-    .drop(gkgDs.col("Counts"))
-    .drop(gkgDs.col("V2Counts"))
-    .drop(gkgDs.col("Locations"))
-    .drop(gkgDs.col("V2Locations"))
-    .drop(gkgDs.col("Persons"))
-    .drop(gkgDs.col("V2Persons"))
-    .drop(gkgDs.col("Organizations"))
-    .drop(gkgDs.col("V2Organizations"))
-    .drop(gkgDs.col("Dates"))
-    .drop(gkgDs.col("GCAM"))
-    .drop(gkgDs.col("SharingImage"))
-    .drop(gkgDs.col("RelatedImages"))
-    .drop(gkgDs.col("SocialImageEmbeds"))
-    .drop(gkgDs.col("SocialVideoEmbeds"))
-    .drop(gkgDs.col("Quotations"))
-    .drop(gkgDs.col("AllNames"))
-    .drop(gkgDs.col("Amounts"))
-    .drop(gkgDs.col("TranslationInfo"))
-    .drop(gkgDs.col("Extras"))
+    val gkgRDD = Downloader.zipsToRdd(spark, Context.refPeriod() + "*.gkg.csv.zip")
+    val gkgDs = GKG.rddToDs(spark, gkgRDD)
 
+    logger.info("Launch request c) theme")
+    requestCByTheme(spark, gkgDs)
 
-  val newDs2 = themeDs
-    .withColumn("Themes2", split(col("Themes"), ";"))
-    .withColumn("themesDef", explode(col("Themes2")))
-    .withColumn("temp", split(col("V2Tone"), ","))
-    .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
-    .na.drop()
+    logger.info("Launch request c) person")
+    requestCByPerson(spark, gkgDs)
 
-  //REQ C BY THEME
-  val reqCtheme = newDs2
-    .groupBy("DATE","SourceCommonName","themesDef")
-    .agg(count("GKGRECORDID").alias("nbArticle"),mean("V2ToneMean").alias("toneMean"))
+    logger.info("Launch request c) country")
+    requestCByCountry(spark, gkgDs)
 
+    logger.info("Fu-fu program completed")
+  }
+  catch {
+    // The call was transmitted successfully, but AWS couldn't process
+    // it, so it returned an error response.
+    case e: AmazonServiceException => e.printStackTrace();
+    // Amazon S3 couldn't be contacted for a response, or the client
+    // couldn't parse the response from AWS.
+    case e: SdkClientException => e.printStackTrace();
+  }
 
-  reqCtheme.write.mode("overwrite").csv(Context.outputPath + "/reqCtheme_csv")
+  // REQUEST C BY THEME
+  def requestCByTheme(spark: SparkSession, gkgDs: Dataset[GKG]) = {
 
-  val personDs: DataFrame = gkgDs
-    .drop(gkgDs.col("SourceCollectionIdentifier"))
-    .drop(gkgDs.col("DocumentIdentifier"))
-    .drop(gkgDs.col("Counts"))
-    .drop(gkgDs.col("V2Counts"))
-    .drop(gkgDs.col("Locations"))
-    .drop(gkgDs.col("V2Locations"))
-    .drop(gkgDs.col("Themes"))
-    .drop(gkgDs.col("V2Themes"))
-    .drop(gkgDs.col("Organizations"))
-    .drop(gkgDs.col("V2Organizations"))
-    .drop(gkgDs.col("Dates"))
-    .drop(gkgDs.col("GCAM"))
-    .drop(gkgDs.col("SharingImage"))
-    .drop(gkgDs.col("RelatedImages"))
-    .drop(gkgDs.col("SocialImageEmbeds"))
-    .drop(gkgDs.col("SocialVideoEmbeds"))
-    .drop(gkgDs.col("Quotations"))
-    .drop(gkgDs.col("AllNames"))
-    .drop(gkgDs.col("Amounts"))
-    .drop(gkgDs.col("TranslationInfo"))
-    .drop(gkgDs.col("Extras"))
+    val themeDs: DataFrame = gkgDs
+      //.drop(gkgDs.col("SourceCollectionIdentifier"))
+      .drop("DocumentIdentifier", "Counts", "V2Counts", "Locations", "V2Locations", "Persons", "V2Persons",
+        "Organizations", "V2Organizations", "Dates", "GCAM", "SharingImage", "RelatedImages", "SocialImageEmbeds",
+        "SocialVideoEmbeds", "Quotations", "AllNames", "Amounts", "TranslationInfo", "Extras")
 
-  val personDs2 = personDs
-    .withColumn("persons2", split(col("Persons"), ";"))
-    .withColumn("personsDef", explode(col("persons2")))
-    .withColumn("temp", split(col("V2Tone"), ","))
-    .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
-    .na.drop()
+    val newDs2 = themeDs
+      .withColumn("Themes2", split(col("Themes"), ";"))
+      .withColumn("themesDef", explode(col("Themes2")))
+      .withColumn("temp", split(col("V2Tone"), ","))
+      .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
+      .na.drop()
 
-  //REQUETE C BY PERSON
-  val reqCperson = personDs2
-    .groupBy("DATE", "SourceCommonName", "personsDef")
-    .agg(count("GKGRECORDID").alias("nbArtcile"),mean("V2ToneMean").alias("toneMean"))
+    val reqCtheme = newDs2
+      .groupBy("DATE", "SourceCommonName", "themesDef")
+      .agg(count("GKGRECORDID").alias("nbArticle"), mean("V2ToneMean").alias("toneMean"))
+
+    reqCtheme.write.mode("overwrite").csv(Context.outputPath + "/reqCtheme_csv")
+  }
+
+  // REQUEST C BY PERSON
+  def requestCByPerson(spark: SparkSession, gkgDs: Dataset[GKG]) = {
+    val personDs: DataFrame = gkgDs
+      .drop("SourceCollectionIdentifier", "DocumentIdentifier", "Counts", "V2Counts",
+        "Locations", "V2Locations", "Themes", "V2Themes", "Organizations", "V2Organizations",
+        "Dates", "GCAM", "SharingImage", "RelatedImages", "SocialImageEmbeds", "SocialVideoEmbeds", "Quotations",
+        "AllNames", "Amounts", "TranslationInfo", "Extras")
+
+    val personDs2 = personDs
+      .withColumn("persons2", split(col("Persons"), ";"))
+      .withColumn("personsDef", explode(col("persons2")))
+      .withColumn("temp", split(col("V2Tone"), ","))
+      .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
+      .na.drop()
+
+    val reqCperson = personDs2
+      .groupBy("DATE", "SourceCommonName", "personsDef")
+      .agg(count("GKGRECORDID").alias("nbArtcile"), mean("V2ToneMean").alias("toneMean"))
 
 
-  reqCperson.write.mode("overwrite").csv(Context.outputPath + "/reqCperson_csv")
+    reqCperson.write.mode("overwrite").csv(Context.outputPath + "/reqCperson_csv")
+  }
 
-  val countryDs: DataFrame = gkgDs
-    .drop(gkgDs.col("SourceCollectionIdentifier"))
-    .drop(gkgDs.col("DocumentIdentifier"))
-    .drop(gkgDs.col("Counts"))
-    .drop(gkgDs.col("V2Counts"))
-    .drop(gkgDs.col("Persons"))
-    .drop(gkgDs.col("V2Persons"))
-    .drop(gkgDs.col("Themes"))
-    .drop(gkgDs.col("V2Themes"))
-    .drop(gkgDs.col("Organizations"))
-    .drop(gkgDs.col("V2Organizations"))
-    .drop(gkgDs.col("Dates"))
-    .drop(gkgDs.col("GCAM"))
-    .drop(gkgDs.col("SharingImage"))
-    .drop(gkgDs.col("RelatedImages"))
-    .drop(gkgDs.col("SocialImageEmbeds"))
-    .drop(gkgDs.col("SocialVideoEmbeds"))
-    .drop(gkgDs.col("Quotations"))
-    .drop(gkgDs.col("AllNames"))
-    .drop(gkgDs.col("Amounts"))
-    .drop(gkgDs.col("TranslationInfo"))
-    .drop(gkgDs.col("Extras"))
+  // REQUEST C BY COUNTRY
+  def requestCByCountry(spark: SparkSession, gkgDs: Dataset[GKG]) = {
+    val countryDs: DataFrame = gkgDs
+      .drop("SourceCollectionIdentifier", "DocumentIdentifier", "Counts", "V2Counts",
+        "Persons", "V2Persons", "Themes", "V2Themes", "Organizations", "V2Organizations", "Dates", "GCAM",
+        "SharingImage", "RelatedImages", "SocialImageEmbeds", "SocialVideoEmbeds", "Quotations", "AllNames", "Amounts",
+        "TranslationInfo", "Extras")
 
+    val CombinationsCountriesUdf = udf(CombinationsCountries _)
+
+    val countryDs2 = countryDs
+      .withColumn("michel", CombinationsCountriesUdf(col("V2Locations")))
+      .withColumn("country", explode(col("michel")))
+      .withColumn("temp", split(col("V2Tone"), ","))
+      .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
+      .na.drop()
+
+    val reqCcountry = countryDs2
+      .groupBy(col("DATE"), col("SourceCommonName"), col("country"))
+      .agg(count("GKGRECORDID").alias("nbArticle"), mean("V2ToneMean").alias("toneMean"))
+
+    reqCcountry.write.mode("overwrite").csv(Context.outputPath + "reqCcountry_csv")
+  }
+
+  // UDF to extract country from location
   def CombinationsCountries(string_countries: String): List[String] = {
     /**
      *
@@ -141,23 +135,4 @@ object MainQueryC extends App {
     }
     countries.toList
   }
-
-
-  val CombinationsCountriesUdf = udf(CombinationsCountries _)
-
-  val countryDs2 = countryDs
-    .withColumn("michel", CombinationsCountriesUdf(col("V2Locations")))
-    .withColumn("country", explode(col("michel")))
-    .withColumn("temp", split(col("V2Tone"), ","))
-    .withColumn("V2ToneMean", col("temp")(0).cast("Float"))
-    .na.drop()
-
-
-  val reqCcountry = countryDs2
-    .groupBy(col("DATE"),col("SourceCommonName"),col("country"))
-    .agg(count("GKGRECORDID").alias("nbArticle"),mean("V2ToneMean").alias("toneMean"))
-
-
-  reqCcountry.write.mode("overwrite").csv(Context.outputPath + "reqCcountry_csv")
-                           
 }
